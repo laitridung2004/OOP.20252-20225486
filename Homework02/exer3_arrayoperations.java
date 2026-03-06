19f6dd1c636dd5e255f3a2e78db3d03f5ab9061b
Lại Trí Dũng <laitridung2004@gmail.com>
2026-03-07 01:32:24 +0700

    upload exercise 3

---
+++ Homework02/exer3_arrayoperations.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class exer3_arrayoperations {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Number of elements:");
+            int n = scanner.nextInt();
+            if (n <= 0) {
+                System.out.println("Size must be greater than 0");
+                return;
+            }
+            double[] array = new double[n];
+            System.out.println("Enter " + n + " numbers:");
+            for (int i = 0; i < n; i++) {
+                array[i] = scanner.nextDouble();
+            }
+            Arrays.sort(array);
+            double sum = 0;
+            for (double num : array) {
+                sum += num;
+            }
+            double average = sum / n;
+            System.out.println("sorted array: " + Arrays.toString(array));
+            System.out.println("sum: " + sum);
+            System.out.println("average: " + average);
+        }
+    }
+}
